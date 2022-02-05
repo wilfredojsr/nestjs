1ce22389b6809658aa391827bd1def0e362b7cfd
Wilfredo Suarez <wilfredojsr2@gmail.com>
2022-02-04 22:17:46 -0300

    section_2

---
+++ iluvcoffees/src/coffees/coffees.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Body,
+  Controller, Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param, Patch,
+  Post, Query, Res,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+  constructor(private readonly coffeeService: CoffeesService) {}
+
+  @Get()
+  findAll(@Query() paginationQuery) {
+    // const { limit, offset } = paginationQuery;
+    return this.coffeeService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.coffeeService.findOne(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createBodyDto: CreateCoffeeDto) {
+    return this.coffeeService.create(createBodyDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeeService.update(id, updateCoffeeDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.coffeeService.remove(id);
+  }
+}
